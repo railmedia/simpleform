75ae3c5be5b5c7f90a3115876fd9300b02be2b24
railmedia <adrian@railmedia.ro>
2020-06-19 09:07:28 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -11,29 +11,42 @@
 <h1>Simple Form</h1>
 <?php
 if( isset( $_POST['form_submitted'] ) ) {
-    $validation = array('first_name' => 'First Name', 'last_name' => 'Last Name', 'email' => 'E-mail');
-    foreach( $validation as $field => $label ) {
+
+    $fields = array('first_name' => 'First Name', 'last_name' => 'Last Name', 'email' => 'E-mail');
+    foreach( $fields as $field => $label ) {
         if( ! isset( $_POST[ $field ] ) || ! $_POST[ $field ] ) {
             echo '<p><font color="red">' . $label . ' is required</font></p>';
         }
     }
 
-    $files = array('pdf_1', 'pdf_2');
-    foreach( $files as $file ) {
+    echo '<h2>Uploaded Data</h2>';
+
+    foreach( $fields as $field => $label ) {
+        echo '<p><strong>' . $label . '</strong>: ' . $_POST[$field] . '</p>';
+    }
+
+    $files = array('pdf_1' => 'PDF 1', 'pdf_2' => 'PDF 2');
+    foreach( $files as $file => $label ) {
+        // if( ! isset( $_POST[$file] ) ) {
+        //     echo '<p><font color="orange">You did not upload the file ' . $label . '</font></p>';
+        //     continue;
+        // }
         $target_dir = "./";
         $_FILES[$file]["name"] =  strtolower( str_replace(' ', '-', $_POST['first_name'] ) ) . '_' . $_POST['suffix'] . '.pdf';
         $target_file = $target_dir . basename($_FILES[$file]["name"]);
-        echo '<p>' . basename( $_FILES[$file]["name"]) . '</p>';
+        //echo '<p>' . basename( $_FILES[$file]["name"]) . '</p>';
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
         if (move_uploaded_file($_FILES[$file]["tmp_name"], $target_file)) {
-           echo '<p><font color="green">The file ' . basename( $_FILES[$file]["name"]) . ' has been uploaded.</font></p>';
+           echo '<p><font color="green">The file ' . basename( $_FILES[$file]["name"]) . ' has been uploaded. File URL is <a target="_blank" href="https://www.tudorache.me/simpleform/' . $_FILES[$file]["name"] . '">https://www.tudorache.me/simpleform/' . $_FILES[$file]["name"] . '</a></font></p>';
          } else {
            echo '<p><font color="red">Error uploading file in field ' . $file . '</font></p>';
          }
     }
-
-}
+?>
+    <p><a href="/simpleform">Start Over</a></p>
+<?php
+} else {
 ?>
 <form enctype="multipart/form-data" method="POST" action="">
     <p>
@@ -64,5 +77,6 @@ if( isset( $_POST['form_submitted'] ) ) {
         <button type="submit" name="form_submitted">Send</button>
     </p>
 </form>
+<?php } ?>
 </body>
 </html>
