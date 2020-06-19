7d1e38966064b55bf25c9255e49bfbc2352b1f0c
railmedia <adrian@railmedia.ro>
2020-06-19 09:08:53 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -26,22 +26,20 @@ if( isset( $_POST['form_submitted'] ) ) {
     }
 
     $files = array('pdf_1' => 'PDF 1', 'pdf_2' => 'PDF 2');
+
     foreach( $files as $file => $label ) {
-        // if( ! isset( $_POST[$file] ) ) {
-        //     echo '<p><font color="orange">You did not upload the file ' . $label . '</font></p>';
-        //     continue;
-        // }
+
         $target_dir = "./";
         $_FILES[$file]["name"] =  strtolower( str_replace(' ', '-', $_POST['first_name'] ) ) . '_' . $_POST['suffix'] . '.pdf';
+
         $target_file = $target_dir . basename($_FILES[$file]["name"]);
-        //echo '<p>' . basename( $_FILES[$file]["name"]) . '</p>';
-        $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
         if (move_uploaded_file($_FILES[$file]["tmp_name"], $target_file)) {
-           echo '<p><font color="green">The file ' . basename( $_FILES[$file]["name"]) . ' has been uploaded. File URL is <a target="_blank" href="https://www.tudorache.me/simpleform/' . $_FILES[$file]["name"] . '">https://www.tudorache.me/simpleform/' . $_FILES[$file]["name"] . '</a></font></p>';
-         } else {
-           echo '<p><font color="red">Error uploading file in field ' . $file . '</font></p>';
-         }
+            echo '<p><font color="green">The file ' . basename( $_FILES[$file]["name"]) . ' has been uploaded. File URL is <a target="_blank" href="https://www.tudorache.me/simpleform/' . $_FILES[$file]["name"] . '">https://www.tudorache.me/simpleform/' . $_FILES[$file]["name"] . '</a></font></p>';
+        } else {
+            echo '<p><font color="red">Error uploading file in field ' . $file . '</font></p>';
+        }
+
     }
 ?>
     <p><a href="/simpleform">Start Over</a></p>
